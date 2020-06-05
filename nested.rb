65c073c84001a69c94457a07284fa815d2951a23
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-05 02:43:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -120,7 +120,7 @@ def changing_alan
       }
     }
     
-    programmer_hash.merge[:alan_kay][:known_for] = "Gui"
+    programmer_hash.merge[:alan_kay][:known_for] = alans_new_info
     return programmer_hash
 end
 
